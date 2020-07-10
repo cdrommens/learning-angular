27c050388862c75f58521e59df41c1f3e02646a8
Cederik Rommens <cederik.rommens@kbc.be>
2020-07-10 15:37:08 +0200

    close dropdowns on click outside

--- angular/course-project/src/app/shared/dropdown.directive.ts
+++ angular/course-project/src/app/shared/dropdown.directive.ts
@@ -1,17 +1,36 @@
 import { Directive, HostListener, HostBinding, ElementRef, Renderer2 } from '@angular/core';
+import { Console } from 'console';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-
-  @HostListener('click') toggleOpen() {
-    if (this.elementRef.nativeElement.classList.contains('show')) {
-      this.renderer.removeClass(this.elementRef.nativeElement, 'show');
-      this.renderer.removeClass(this.elementRef.nativeElement.childNodes[1], 'show');
+  // when using click on this element: the dropdown only closes when you click on the dropdown again, so not outside of the dropdown:
+  // @HostListener('click') toggleOpen() {
+  // if you want that behaviour, you have to bind the click to the document
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // check if we clicked on the button
+    if (this.elementRef.nativeElement.contains(event.target)) {
+      if (this.elementRef.nativeElement.classList.contains('show')) {
+        this.renderer.removeClass(this.elementRef.nativeElement, 'show');
+        this.renderer.removeClass(
+          this.elementRef.nativeElement.childNodes[1],
+          'show'
+        );
+      } else {
+        this.renderer.addClass(this.elementRef.nativeElement, 'show');
+        this.renderer.addClass(
+          this.elementRef.nativeElement.childNodes[1],
+          'show'
+        );
+      }
     } else {
-      this.renderer.addClass(this.elementRef.nativeElement, 'show');
-      this.renderer.addClass(this.elementRef.nativeElement.childNodes[1], 'show');
+      // if we didn't click on the button, close all dropdowns
+      this.renderer.removeClass(this.elementRef.nativeElement, 'show');
+      this.renderer.removeClass(
+        this.elementRef.nativeElement.childNodes[1],
+        'show'
+      );
     }
   }
 
